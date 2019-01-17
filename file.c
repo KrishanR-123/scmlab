c1b128eeae43ea40d6a0c414fa2f35324c4ef798
Krishan Rupani <krrish.rup25@gmail.com>
2019-01-17 10:31:34 +0530

    added skelton cfiles,first commit

--- file.c
+++ file.c
@@ -4,7 +4,9 @@ int main()
   int a,b,c;
   printf("enter any two numbers");
   scanf("%d%d",a,b);
-  printf("sum of your numbers is %d",a+b);
+  c=a+b;
+  printf("sum of your numbers is %d",c);
+
   return 0;
 }
 
